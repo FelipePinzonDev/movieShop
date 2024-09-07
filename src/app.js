a4a7c76ff6d025912e892fae0045e3e0ea9933f8
unknown <fepinzon77@gmail.com>
2024-09-07 15:55:18 -0500

    [ADD] First commit with docker & mongo &Express v0.4

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const express = require('express')
-const { config } = require('dotenv')
 const mongoose = require('mongoose')
+const { config } = require('dotenv')
 const bodyParser = require('body-parser')
-const movieRoutes = require('./routes/movies.routes')
 
 config()
+const movieRoutes = require('./routes/movies.routes')
 
 const app = express()
 app.use(bodyParser.json())
 
 mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME })
-
 const db = mongoose.connection
 
 app.use('/movies', movieRoutes)
