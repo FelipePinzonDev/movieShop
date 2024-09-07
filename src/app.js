1a5776cd04152477911ca612f02928180d97a39e
unknown <fepinzon77@gmail.com>
2024-09-07 15:40:15 -0500

    [ADD] First commit with docker & mongo &Express v0.2

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ config()
 const app = express()
 app.use(bodyParser.json())
 
-mongoose.connect(process.env.MONGO_URI, { dbName: process.env.MONGO_DB_NAME })
+mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME })
 
 const db = mongoose.connection
 
